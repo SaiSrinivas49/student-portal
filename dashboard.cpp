aedef5066dd106ababee5cd312a3792082d3e7e5
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-16 03:56:29 +0530

    Added info

--- dashboard.cpp
+++ dashboard.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main() {
-    cout << " Student Dashboard " << endl;
+    cout << "===== Student Dashboard =====" << endl;
     cout << "Welcome to Student Portal" << endl;
 
-    cout << "Student Summary" << endl;
+    cout << "\nStudent Summary" << endl;
     cout << "Student ID: STU001" << endl;
     cout << "Name: Student" << endl;
+
+    cout << "\nAcademic Information" << endl;
+    cout << "Semester: 4" << endl;
+    cout << "Attendance: 92%" << endl;
 
     return 0;
 }
