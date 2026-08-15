31388844a3f0786a5e41bb13a08a6e6a6cc3e8be
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-15 16:23:23 +0530

    Added login.cpp

---
+++ login.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string username;
+
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Welcome, " << username << "!" << endl;
+
+    return 0;
+}
