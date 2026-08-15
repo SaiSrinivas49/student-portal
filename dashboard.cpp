ffd1828144a87bd8cb29cd1b470c30ed9f314c45
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-16 04:59:22 +0530

    Added student dashboard

--- dashboard.cpp
+++ dashboard.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    cout << "===== Student Dashboard =====" << endl;
-    cout << "Welcome to Student Portal" << endl;
+    
+    cout << "Welcome to Student Portal - Student View" << endl;
 
     cout << "\nStudent Summary" << endl;
     cout << "Student ID: STU001" << endl;
