e1ed0a8a32a7e63216bf3a2ad3bf30f7c4380085
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-15 12:40:47 +0530

    Added semester to student info

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main()
 {
     int id;
     string name;
+    int semester;
     string department;
 
     cout << "Enter Student ID: " << endl;
@@ -25,9 +26,18 @@ int main()
     cout << "Enter Department: " << endl;
     getline(cin, department);
 
+    cout << "Enter Semester: ";
+    cin >> semester;
+
+    while (semester < 1 || semester > 8){
+        cout << "Semester must be between 1 and 8. Enter again: " << endl;
+        cin >> semester;
+    }
+
     cout << "Student Details " << endl;
     cout << "ID: " << id << endl;
     cout << "Name: " << name << endl;
+    cout << "Semester: " << semester << endl;
     cout << "Department: " << department << endl;
 
     return 0;
