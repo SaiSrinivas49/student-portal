5333e76ff85b9c8a23a1be45f1c4908a1b38f790
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-16 04:12:47 +0530

    Added privacy settings

--- settings.cpp
+++ settings.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() {
-    cout << " Student Portal Settings " << endl;
+    cout << "===== Student Portal Settings =====" << endl;
 
     cout << "\nNotification Settings" << endl;
     cout << "Email notifications: Enabled" << endl;
     cout << "Assignment reminders: Enabled" << endl;
+
+    cout << "\nPrivacy Settings" << endl;
+    cout << "Profile visibility: Private" << endl;
+    cout << "Email visibility: Private" << endl;
 
     return 0;
 }
