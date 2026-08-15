5e7213040bbeb8981052e529870189315c62d8c0
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-16 04:08:36 +0530

    Added modifications

--- settings.cpp
+++ settings.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main() {
     cout << " Student Portal Settings " << endl;
-    cout << "Settings menu opened." << endl;
+
+    cout << "\nNotification Settings" << endl;
+    cout << "Email notifications: Enabled" << endl;
+    cout << "Assignment reminders: Enabled" << endl;
 
     return 0;
 }
