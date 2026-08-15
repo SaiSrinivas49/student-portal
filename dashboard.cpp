00ed50f45435f887dbc2d96e59555a6e42900f6b
SaiSrinivas49 <ksaisrinivas49@gmail.com>
2026-08-16 03:53:12 +0530

    Added summary to dashboard

--- dashboard.cpp
+++ dashboard.cpp
@@ -5,5 +5,9 @@ int main() {
     cout << " Student Dashboard " << endl;
     cout << "Welcome to Student Portal" << endl;
 
+    cout << "Student Summary" << endl;
+    cout << "Student ID: STU001" << endl;
+    cout << "Name: Student" << endl;
+
     return 0;
 }
